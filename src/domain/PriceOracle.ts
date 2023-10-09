a12d9ecd7b683e54e16ac5afde3cc1747e9e951b
Fer <ferp@protonmail.ch>
2023-10-09 18:25:02 +0200

    separate responsibilities in modules

---
+++ src/domain/PriceOracle.ts
@@ -0,0 +1,91 @@
+import { ethers } from "ethers";
+import axios from "axios";
+import Big from "big.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { provider, uniswapV3PoolContract, nonFungiblePositionManagerContract, erc20Abi } from "./contracts";
+
+
+// pool contract
+export const getPoolPrice = async (): Promise<Big> => {
+  const slot0 = await uniswapV3PoolContract.slot0();
+  const sqrtRatioX96 = slot0.sqrtPriceX96.toString();
+  const price = new Big(sqrtRatioX96).pow(2).div(2 ** 192);
+  return price;
+};
+
+// pool and token contracts
+export const getDecimals = async (): Promise<number[]> => {
+  const token0address = await uniswapV3PoolContract.token0();
+  const token1address = await uniswapV3PoolContract.token1();
+
+  const token0Contract = new ethers.Contract(
+    token0address,
+    erc20Abi.abi,
+    provider
+  );
+
+  const token1Contract = new ethers.Contract(
+    token1address,
+    erc20Abi.abi,
+    provider
+  );
+
+  const token0Decimals = await token0Contract.decimals();
+  const token1Decimals = await token1Contract.decimals();
+
+  return [token0Decimals, token1Decimals];
+};
+
+// pool, and token contracts
+export const getInvertedPrice = async (price: Big): Promise<Big> => {
+  // consider the decimals of each token
+  const [token0Decimals, token1Decimals] = await getDecimals();
+  const decimalsDifference = Number(token1Decimals - token0Decimals);
+  const invertedPrice = price.pow(-1).mul(Big(10).pow(decimalsDifference));
+  return invertedPrice;
+};
+
+// third-party api
+export const getEthPrice = async (): Promise<number> => {
+  const ethPriceResponse = await axios.get(
+    "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+  );
+  const ethPriceData = ethPriceResponse.data as {
+    ethereum: { usd: number };
+  };
+  return ethPriceData.ethereum.usd;
+};
+
+// position manager contract
+export const getPositionTicks = async (positionId: number) => {
+  const position = await nonFungiblePositionManagerContract.positions(positionId);
+  const tickLower = position.tickLower.toString();
+  const tickUpper = position.tickUpper.toString();
+  return { tickLower, tickUpper };
+};
+
+export const tickToPrice = (tick: string): number => {
+  const price = Math.pow(1.0001, parseInt(tick));
+  return price;
+};
+
+export const printData = async () => {
+  const poolPrice = await getPoolPrice();
+  const invertedPoolPrice = await getInvertedPrice(poolPrice);
+  const price = await getEthPrice();
+  console.log(`Pool price: ${Number(poolPrice)}`);
+  console.log(`inverted Pool price ${Number(invertedPoolPrice)}`);
+  console.log(`ETH price: ${price}`);
+  const { tickLower, tickUpper } = await getPositionTicks(574861);
+  const lowerPrice = tickToPrice(tickLower);
+  const upperPrice = tickToPrice(tickUpper);
+  console.log(`lower price: ${lowerPrice}`);
+  console.log(`upper price: ${upperPrice}`);
+  const token1PriceLower = await getInvertedPrice(Big(lowerPrice));
+  const token1PriceUpper = await getInvertedPrice(Big(upperPrice));
+  console.log(`token1 price lower: ${token1PriceLower}`);
+  console.log(`token1 price upper: ${token1PriceUpper}`);
+  return "done";
+};
---
+++ src/domain/contracts.ts
@@ -0,0 +1,30 @@
+import { ethers } from "ethers";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const provider = new ethers.WebSocketProvider(
+  `wss://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+);
+
+// pool for uscd/eth at 0.05% fee tier
+const UniV3PoolAddress = process.env.POOL_ADDRESS || "";
+const uniswapV3PoolAbi = require("@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json");
+
+// non fungible position manager
+const nonFungiblePositionManagerAddress = process.env.POSITION_MANAGER_ADDRESS || "";
+const nonFungiblePositionManagerAbi = require("@uniswap/v3-periphery/artifacts/contracts/interfaces/INonfungiblePositionManager.sol/INonfungiblePositionManager.json");
+
+// get contract for erc20
+export const erc20Abi = require("@uniswap/v3-periphery/artifacts/contracts/interfaces/IERC20Metadata.sol/IERC20Metadata.json");
+
+export const uniswapV3PoolContract = new ethers.Contract(
+  UniV3PoolAddress,
+  uniswapV3PoolAbi.abi,
+  provider
+);
+
+export const nonFungiblePositionManagerContract = new ethers.Contract(
+  nonFungiblePositionManagerAddress,
+  nonFungiblePositionManagerAbi.abi,
+  provider
+);
