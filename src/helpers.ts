179f01f640ee12d4ce41b21451eb7d767bc8afcc
Fer <ferp@protonmail.ch>
2023-10-09 17:44:32 +0200

    helper to calculate impermanent loss when playing with only stablecoin

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,11 +1,31 @@
-function imperLoss (newPrice: number, oldPrice: number, lowerPriceLimit: number, higherPriceLimit: number): number {
-	const k = newPrice / oldPrice
-	
-	const numerator = (2 * k ** 0.5 - 1 - k)
-	const denominator = 1 + k - (lowerPriceLimit/oldPrice) ** 0.5 - k * ( oldPrice/higherPriceLimit) ** 0.5
-	
-	const lossRatio = numerator/denominator
-	
-	return lossRatio
-	}
-	
+// source   https://medium.com/auditless/impermanent-loss-in-uniswap-v3-6c7161d3b445
+function imperLoss(
+  newPrice: number,
+  oldPrice: number,
+  lowerPriceLimit: number,
+  higherPriceLimit: number
+): number {
+  const k = newPrice / oldPrice;
+
+  const numerator = 2 * k ** 0.5 - 1 - k;
+  const denominator =
+    1 +
+    k -
+    (lowerPriceLimit / oldPrice) ** 0.5 -
+    k * (oldPrice / higherPriceLimit) ** 0.5;
+
+  const lossRatio = numerator / denominator;
+
+  return lossRatio;
+}
+
+// higherPriceLimit = 1
+// oldPrice = higherPriceLimit
+// ratioBelow = newPrice / oldPrice = newPrice / higherPriceLimit = newPrice / 1 = newPrice
+// invervalRatioBelow = lowerPriceLimit / higherPriceLimit = lowerPriceLimit / oldPrice = lowerPriceLimit / 1 = lowerPriceLimit
+function imperLossBelowStablecoin(
+  ratioBelow: number,
+  intervalRatioBelow: number
+): number {
+  return imperLoss(ratioBelow, 1, intervalRatioBelow, 1);
+}
